28b27148b7aefc6f72327214fa48c9417025e454
saditahmid <1820228@iub.edu.bd>
2021-06-27 02:36:02 +0600

    add json server

--- src/containers/Homepage/index.jsx
+++ src/containers/Homepage/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { TopSection } from "./topSection";
+import { Navbar } from "../../components/navbar";
 export function Homepage(props){
-    return <pageContainer><TopSection/  >
+    return <pageContainer>
+        <TopSection >
+            <Navbar/>
+            </TopSection>
 </pageContainer>
 }
