0d3543ada3361d8ccd6c20b21c2cdebf8e33427e
saditahmid <1820228@iub.edu.bd>
2021-06-30 02:27:08 +0600

    Merge branch 'master' of https://github.com/saditahmid/react-servycing

--- src/components/serviceCard/index.jsx
+++ src/components/serviceCard/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import { Marginer } from "../marginer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 const CardContainer = styled.div`
 width : 300 px;
@@ -32,11 +35,84 @@ width: 100%
 display: flex;
 flex-direction: column;
 flex: 1;
-padding: 0 10 px;
-
+align-items: flex-start;
+padding: 15px 14px;
 
 `;
 
-export function serviceCard(props){
+const BottomContainer = styled.div`
+width: 100%;
+height: 32px;
+display: flex;
+align-items: center;
+justify-content: space-between;
+border-top: 1px solid rgba(15,15,15,0.19);
+padding: 0 10px;
 
+`;
+
+const Title = styled.h2`
+margin: 0;
+font-size: 24px;
+color: #000;
+font-weight: 500;
+
+`;
+
+const specialistName = styled.h4`
+margin: 0;
+color: rgba(150,151,151,1);
+font-size: 12px;
+font-weight: 400;
+`;
+
+const RatingContainer=styled.div`
+display: flex;
+color: #EBE204;
+`;
+const StaringAtText = styled.h6`
+margin: 0;
+color: rgba(161,161,161,0.9);
+font-weight: 400;
+`;
+
+const PriceContainer =styled.div`
+display: flex;
+rgba(161,161,161,0.9)
+`;
+
+const PriceText=styled.div`
+margin-left: 3px;
+color: #2A9D8F;
+font-weight:500;
+`;
+
+
+export function serviceCard(props){
+    const { thumbnailUrl,specialist,id,title,rate, rating} = props;
+
+    return<CardContainer>
+        <TopContainer>
+            <ServiceThumbnail>
+            <img src={thumbnailUrl} alt="{title}"/>
+            </ServiceThumbnail>
+        </TopContainer>
+        <ContentContainer>
+            <Title>{title}</Title>
+            <Marginer direction= "vertical" Margin={10}/>
+        <specialistName>
+            {specialist.fullName}
+        </specialistName>
+        </ContentContainer>
+        <BottomContainer>
+            <RatingContainer>
+                <FontAwesomeIcon icon= {faStar} size= "sm"/>
+                {rating}
+            </RatingContainer>
+            <PriceContainer>
+                <StaringAtText>Starting at </StaringAtText>
+                <PriceText> ${ rating}/hr </PriceText>
+            </PriceContainer>
+        </BottomContainer>
+    </CardContainer>
 }
--- src/containers/Homepage/index.jsx
+++ src/containers/Homepage/index.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import styled from "styled-components";
+import { serviceCard } from "../../components/serviceCard"
 import { TopSection } from "./topSection";
+import { InnerPageContainer, PageContainer } from "../../components/pageContainer"
 import { Navbar } from "../../components/navbar";
+import { deviceSize } from "../../components/responsive";
+const Title = styled.h1`
+font-weight: 900;
+color: #000;
+`;
+
+const ContentContainer =styled.div`
+width: 100;
+max-width: $(deviceSize.laptop)px;
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+`;
+
+const service = { "id": 1, "title": "I will landscape your garden", 
+"thumbnailUrl": "http://localhost:9000/garden.jpg", "rate": 34,
+"rating": 4, "specialist": { "id": 1, "fullName": "Mark brone" },
+}
+
+
 export function Homepage(props){
     return <pageContainer>
         <TopSection >
             <Navbar/>
             </TopSection>
+            <InnerPageContainer>
+               <ContentContainer>
+                   <Title>
+                    Most used services & more
+                   </Title>
+                   <serviceCard { ...service} />
+               </ContentContainer>
+            </InnerPageContainer>
 </pageContainer>
 }
